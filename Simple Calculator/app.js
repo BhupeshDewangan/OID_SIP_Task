33712887185d3f16bedc23f1a8b2a891acf80351
Bhupesh Kumar Dewangan <84950520+BhupeshDewangan@users.noreply.github.com>
2022-07-23 19:37:31 +0530

    Add files via upload

---
+++ Simple Calculator/app.js
@@ -0,0 +1,83 @@
+let equal_pressed = 0;
+// refer all buttons excluding AC and DEL
+
+let button_input = document.querySelectorAll(
+    ".input-button");
+
+// refer input, equal, clear, and erase
+
+let input = document.getElementById("input");
+let equal = document.getElementById("equal");
+let clear = document.getElementById("clear");
+let erase = document.getElementById("erase");
+
+window.onload = () => 
+{
+    input.value = "";
+};
+
+// access each class using foreach
+button_input.forEach((button_class) =>
+{
+    button_class.addEventListener("click", ()=>
+    {
+        if(equal_pressed == 1)
+        {
+            input.value = "";
+            equal_pressed = 0;
+        }
+
+        // display value of each button
+        input.value += button_class.value;
+    });
+});
+
+// solve the user input when clicked on equal sign
+equal.addEventListener("click", ()=> 
+{
+    equal_pressed = 1;
+
+    let input_val = input.value;
+
+    try
+    {
+        // evaluate users input 
+
+        let solution = eval(input_val);
+
+        // true for natural No.
+        // false for decimal
+
+        if(Number.isInteger(solution))
+        {
+            input.value = solution;
+        }
+
+        else{
+            input.value = solution.toFixed(2);
+        }   
+    }
+
+    catch(err)
+    {
+         
+        // if user has entered invalid input
+
+        alert("Invalid Input");
+    }
+});
+
+
+// clear whole input
+
+clear.addEventListener("click", ()=> 
+{
+    input.value = "";
+})
+
+// erase single digit
+
+erase.addEventListener("click", () => 
+{
+    input.value = input.value.substr(0, input.value.length - 1);
+}) ;
